f10c45303c91650b8490bf1bcb4442574be803be
Duy Do <juchiast@gmail.com>
2021-02-22 21:43:20 +0700

    use example

--- code/sonic/src/bin/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let c = sonic::uint32::sha256();
-    println!("{}", sonic::uint32::collect_vertices(c.clone()).len());
-    let c = sonic::circuit::convert(c);
-    println!("{}", sonic::circuit::toposort(c.clone()).len());
-}
